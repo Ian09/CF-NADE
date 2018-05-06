429899193a4aa5ef09def6d912a106b6f0d88b37
Dynsk <s.ni@columbia.edu>
2018-05-05 20:34:33 -0400

    Data_user_per_sample.py: roll back to no shuffle

--- old_implement/Data_user_per_sample.py
+++ old_implement/Data_user_per_sample.py
@@ -30,8 +30,6 @@ class Data_user():
         self.output_mask = np.ones((self.user, self.M), dtype=bool)
         self.input_mask = np.ones((self.user, self.M), dtype=bool)
 
-
-    def prepare_data(self):
         i = 0
         keys = list(self.userList.keys())
         print(type(keys))
@@ -102,7 +100,6 @@ class Data_user():
     def renew_train(self):
         self.used_train = 0
         rd.shuffle(self.index_list_train)
-        self.prepare_data()
 
     def renew_test(self):
         self.used_test = 0
