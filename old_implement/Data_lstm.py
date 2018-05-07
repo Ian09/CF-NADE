b90bc0acc08cf42d0fdd2e6185ebb8e822873777
Dynsk <s.ni@columbia.edu>
2018-05-07 18:03:51 -0400

    Data_lstm.py: complete

--- old_implement/Data_lstm.py
+++ old_implement/Data_lstm.py
@@ -1,6 +1,21 @@
 import random as rd
 import numpy as np
 
+"""
+Use case
+    myData = Data('../ml-1m/ratings.dat')
+    myData.split(0.8)
+    
+    # while training
+    X, Y, _, _ = myData.get_batch_train(512, 20)
+    
+    # after an epoch
+    myData.shuffle_train()
+    myData.renew_train()
+    
+"""
+
+
 class Data:
 
     def __init__(self, data_directory):
@@ -42,7 +57,7 @@ class Data:
         """return X(batch * seq), Y(batch * movie), output_mask(batch * movie), flag"""
         if batch_size + self.used_train >= len(self.train_users):
             return None, None, None, False
-        X, Y = self.construct_matrix(self.train_users[self.used_train : self.used_train+batch_size], sequence_len)
+        X, Y = self.construct_matrix(self.train_users[self.used_train: self.used_train+batch_size], sequence_len, batch_size)
         output_mask = Y > 0
         self.used_train += batch_size
         return X, Y, output_mask, True
@@ -64,13 +79,56 @@ class Data:
         self.used_test = 0
         rd.shuffle(self.test_users)
 
-    def construct_matrix(self, users, seq_len):
+    def rating_coding(self, rating_list, seq_len):
+        """
+        The rating_list is already in time order
+        return a 1 * seq_len matrix
+        """
+        output = np.ones((1, seq_len)) * (-1)
+        if len(rating_list) > seq_len:
+            rating_list = rating_list[len(rating_list) - seq_len:]
+
+        for i in range(seq_len - len(rating_list), seq_len):
+            output[0, i] = self.movie2ratingid(rating_list[i-(seq_len - len(rating_list))][0], rating_list[i- (seq_len - len(rating_list))][1], self.movieDim)
+        return output
+
+    def movie2ratingid(self, movie_id, rating, number_movie):
+        return (rating - 1) * number_movie + self.movieID2index[movie_id]
+
+    def left2vector(self, ratings):
+        """Return a 1 * movie matrix"""
+        output = np.zeros((1, self.movieDim))
+        for rating in ratings:
+            output[0, self.movieID2index[rating[0]]] = rating[1]
+        return output
+
+    def construct_matrix(self, users, seq_len, batch_size):
         """
         :param users: list of keys in self.userList(Dict), which contains all triples of certain user
         :param seq_len:
         :return: X(batch * seq), Y(batch * movie)
         """
-        pass
+        matrix_X = np.zeros((batch_size, seq_len))
+        matrix_Y = np.zeros((batch_size, self.movieDim))
+        i = 0
+        for user in users:
+            splitPoint = rd.randint(1, len(self.userList[user]) - 1)
+            matrix_X[i] = self.rating_coding(self.userList[user][0: splitPoint-1], seq_len)
+            matrix_Y[i] = self.left2vector(self.userList[user][splitPoint-1:])
+            i += 1
+
+        return matrix_X, matrix_Y
+
+
+if __name__ == '__main__':
+
+    myData = Data('../ml-1m/ratings.dat')
+    myData.split(0.8)
+    X, Y, _, _ = myData.get_batch_train(512, 20)
+    myData.shuffle_train()
+    myData.renew_train()
+    X, Y, _, _ = myData.get_batch_train(512, 20)
+
 
 
 
